6b9e804eaad32c0d48159a4005bcbe2004853cf7
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-07-09 12:37:53 +0100

    Part 2: added delete logic to pass tests and await for beforeEach and afterEach

--- src/message/message.data.ts
+++ src/message/message.data.ts
@@ -42,7 +42,6 @@ export class MessageData {
     return chatMessageToObject(message);
   }
 
-
   async getChatConversationMessages(
     data: GetMessageDto,
   ): Promise<PaginatedChatMessages> {
@@ -88,8 +87,13 @@ export class MessageData {
   }
 
   async delete(messageId: ObjectID): Promise<ChatMessage> {
-    // TODO allow a message to be marked as deleted
-    return new ChatMessage() // Minimum to pass ts checks -replace this
+    const message = await this.chatMessageModel.findById(messageId);
+    if (!message) {
+      throw new Error('Message not found');
+    }
+    message.deleted = true;
+    await message.save();
+    return message;
   }
 
   async resolve(messageId: ObjectID): Promise<ChatMessage> {
